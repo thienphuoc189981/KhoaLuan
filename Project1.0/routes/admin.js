7b9fd4f3fbdacc63f6c0d97127960b46a054be64
thienphuoc189981 <12520678@gm.uit.edu.vn>
2016-12-19 21:31:33 +0700

    Admin management: User, Job

--- Project1.0/routes/admin.js
+++ Project1.0/routes/admin.js
@@ -5,6 +5,7 @@ let async = require("async");
 var request = require('request');
 var http = require('http');
 var datetime = require('node-datetime');
+var fs = require('fs');
 function index(req, res) {
     let json = [];
     //if (req.session.privilege == 'admin') {
@@ -13,7 +14,7 @@ function index(req, res) {
             json.push(u.doc);
         });
         res.render('./admin/index', {
-            json: json, layout: 'admin', session: req.session,
+            json: json, layout: 'admin', session: req.session, message: req.flash(),
             helpers: {
                 eq: function (v1) { return v1 == json.status; }
             } });
@@ -29,11 +30,10 @@ function updateUser(req, res) {
     //if (req.session.privilege == 'admin') {
     api.findData(id, 'usersById').then(function (rs) {
         json = rs.rows[0].doc;
-        console.log(json.status);
         res.render('./admin/updateUser', {
-            json: json, layout: 'admin', session: req.session, message: req.flash,
+            json: json, layout: 'admin', session: req.session,
             helpers: {
-                eq: function (v1) { return v1 == json.status; }
+                eq: function (v1) { return v1 == 'Active'; }
             }
         });
     });
@@ -50,7 +50,7 @@ function updateUserSubmit(req, res) {
     api.findData(id, 'usersById').then(function (rs) {
         json = rs.rows[0].doc;
         json.name = body.name;
-        json.email = body.enail;
+        json.email = body.email;
         json.phone = body.phone;
         json.status = body.status;
         api.updateData(json).then(function () {
@@ -64,7 +64,7 @@ function updateUserSubmit(req, res) {
 }
 exports.updateUserSubmit = updateUserSubmit;
 function deleteUser(req, res) {
-    let id = req.body._id;
+    let id = req.params.id;
     //if (req.session.privilege == 'admin') {
     api.findData(id, 'usersById').then(function (rs) {
         api.deleteData(rs.rows[0].doc._id, rs.rows[0].doc._rev).then(function () {
@@ -77,26 +77,93 @@ function deleteUser(req, res) {
     //}
 }
 exports.deleteUser = deleteUser;
-function addUser(req, res) {
+//------jobs-------//
+function managementJobs(req, res) {
+    let json = [];
+    //console.log(req.flash);
     //if (req.session.privilege == 'admin') {
-    res.render('./admin/addUser', {
-        layout: 'admin', session: req.session
+    api.indexView('jobsById').then(function (rs) {
+        rs.rows.forEach(function (u) {
+            json.push(u.doc);
+        });
+        res.render('./admin/managementJobs', {
+            json: json, layout: 'admin', session: req.session, message: req.flash()
+        });
     });
     //} else {
     //    res.redirect('/login');
     //}
 }
-exports.addUser = addUser;
-function insertJob(req, res) {
+exports.managementJobs = managementJobs;
+function deleteJob(req, res) {
+    let id = req.params.id;
     //if (req.session.privilege == 'admin') {
-    let user = req.body;
-    user.type = 'user';
-    res.render('./admin/addUser', {
-        layout: 'admin', session: req.session
+    api.findData(id, 'jobsById').then(function (rs) {
+        api.deleteData(rs.rows[0].doc._id, rs.rows[0].doc._rev).then(function () {
+            req.flash('successUpdate', 'You are successfully deleting !', { maxage: 6000 });
+            res.redirect('/admin/jobs-management');
+        });
     });
     //} else {
     //    res.redirect('/login');
     //}
 }
-exports.insertJob = insertJob;
+exports.deleteJob = deleteJob;
+function updateJob(req, res) {
+    let idJob = req.params.id;
+    let json = [];
+    let cats = [];
+    //if (req.session.privilege == 'admin') {
+    api.findData(idJob, 'jobsById').then(function (result) {
+        api.indexView("categoriesAll").then(function (data) {
+            data.rows.forEach(function (item) {
+                cats.push(item.key);
+            });
+            json.push(result.rows[0].doc);
+            //console.log(json[0]);
+            res.render('./admin/updateJob', {
+                session: req.session, job: json[0], cats: cats, layout: 'admin',
+                helpers: {
+                    eq: function (v1) { return v1 == json[0].category; }
+                }
+            });
+        });
+    });
+    //} else {
+    //    res.redirect('/login');
+    //}
+}
+exports.updateJob = updateJob;
+function updateJobSubmit(req, res) {
+    let idJob = req.body._id;
+    let body = req.body;
+    let json;
+    //if (req.session.privilege == 'admin') {
+    console.log(body.photo);
+    api.findData(idJob, 'jobsById').then(function (job) {
+        json = job.rows[0].doc;
+        if (job.rows[0].doc.attachment && req.file && job.rows[0].doc.attachment != "") {
+            let oldImg = job.rows[0].doc.attachment;
+            fs.unlink(oldImg);
+        }
+        if (req.file) {
+            json.attachment = '/uploads/' + req.file.filename;
+        }
+        console.log(req.file);
+        json.title = body.title;
+        json.description = body.description;
+        json.location = body.location;
+        json.salary = body.salary;
+        json.expireDate = body.expireDate;
+        json.category = body.category;
+        api.updateData(json).then(function () {
+            req.flash('successUpdate', 'You are successfully updating !', { maxage: 6000 });
+            res.redirect('/admin/jobs-management');
+        });
+    });
+    //} else {
+    //    res.redirect('/login');
+    //}
+}
+exports.updateJobSubmit = updateJobSubmit;
 //# sourceMappingURL=admin.js.map
--- Project1.0/routes/admin.ts
+++ Project1.0/routes/admin.ts
@@ -1,9 +1,11 @@
 ﻿import {Project} from "./database";
+import express = require('express');
 let api = new Project.api();
 let async = require("async");
 var request = require('request');
 var http = require('http');
 var datetime = require('node-datetime');
+var fs = require('fs');
 
 
 export function index(req, res) {
@@ -14,7 +16,7 @@ export function index(req, res) {
             json.push(u.doc);
         });
         res.render('./admin/index', {
-            json: json, layout: 'admin', session: req.session,
+            json: json, layout: 'admin', session: req.session, message: req.flash(),
             helpers: {
                 eq: function (v1) { return v1 == json.status; }
             }});
@@ -31,11 +33,10 @@ export function updateUser(req, res) {
     //if (req.session.privilege == 'admin') {
     api.findData(id, 'usersById').then(function (rs) {
         json = rs.rows[0].doc;
-        console.log(json.status);
         res.render('./admin/updateUser', {
-            json: json, layout: 'admin', session: req.session, message: req.flash,
+            json: json, layout: 'admin', session: req.session, 
             helpers: {
-                eq: function (v1) { return v1 == json.status; }
+                eq: function (v1) { return v1 == 'Active'; }
             }
         });
     });
@@ -54,7 +55,7 @@ export function updateUserSubmit(req, res) {
         json = rs.rows[0].doc;
 
         json.name = body.name;
-        json.email = body.enail;
+        json.email = body.email;
         json.phone = body.phone;
         json.status = body.status;
 
@@ -70,7 +71,7 @@ export function updateUserSubmit(req, res) {
 }
 
 export function deleteUser(req, res) {
-    let id = req.body._id;
+    let id = req.params.id;
     //if (req.session.privilege == 'admin') {
     api.findData(id, 'usersById').then(function (rs) {
         api.deleteData(rs.rows[0].doc._id, rs.rows[0].doc._rev).then(function () {
@@ -84,23 +85,95 @@ export function deleteUser(req, res) {
 
 }
 
-export function addUser(req, res) {
+//------jobs-------//
+export function managementJobs(req, res) {
+    let json = [];
+    //console.log(req.flash);
     //if (req.session.privilege == 'admin') {
-    res.render('./admin/addUser', {
-        layout: 'admin', session: req.session
+    api.indexView('jobsById').then(function (rs) {
+        rs.rows.forEach(function (u) {
+            json.push(u.doc);
+        });
+        res.render('./admin/managementJobs', {
+            json: json, layout: 'admin', session: req.session, message: req.flash()
+        });
     });
     //} else {
     //    res.redirect('/login');
     //}
 
-} 
+}
 
-export function insertJob(req, res) {
+export function deleteJob(req, res) {
+    let id = req.params.id;
     //if (req.session.privilege == 'admin') {
-    let user = req.body;
-    user.type = 'user';
-    res.render('./admin/addUser', {
-        layout: 'admin', session: req.session
+    api.findData(id, 'jobsById').then(function (rs) {
+        api.deleteData(rs.rows[0].doc._id, rs.rows[0].doc._rev).then(function () {
+            req.flash('successUpdate', 'You are successfully deleting !', { maxage: 6000 });
+            res.redirect('/admin/jobs-management');
+        });
+    });
+    //} else {
+    //    res.redirect('/login');
+    //}
+
+}
+
+export function updateJob(req, res) {
+    let idJob = req.params.id;
+    let json = [];
+    let cats = [];
+    //if (req.session.privilege == 'admin') {
+        api.findData(idJob, 'jobsById').then(function (result) {
+            api.indexView("categoriesAll").then(function (data) {
+                data.rows.forEach(function (item) {
+                    cats.push(item.key);
+                });
+                json.push(result.rows[0].doc);
+                //console.log(json[0]);
+                res.render('./admin/updateJob', {
+                    session: req.session, job: json[0], cats: cats, layout: 'admin',
+                    helpers: {
+                        eq: function (v1) { return v1 == json[0].category; }
+                    }
+                });
+            });
+        });
+    //} else {
+    //    res.redirect('/login');
+    //}
+
+}
+
+export function updateJobSubmit(req: express.Request, res: express.Response) {
+    let idJob = req.body._id;
+    let body = req.body;
+    let json: any;
+    //if (req.session.privilege == 'admin') {
+    console.log(body.photo);
+    api.findData(idJob, 'jobsById').then(function (job) {
+        json = job.rows[0].doc;
+            if (job.rows[0].doc.attachment && req.file && job.rows[0].doc.attachment != "") {
+                let oldImg = job.rows[0].doc.attachment;
+                fs.unlink(oldImg);
+            }
+
+            if (req.file) {
+                json.attachment = '/uploads/' + req.file.filename;
+            }
+            console.log(req.file);
+
+            json.title = body.title;
+            json.description = body.description;
+            json.location = body.location;
+            json.salary = body.salary;
+            json.expireDate = body.expireDate;
+            json.category = body.category;
+
+            api.updateData(json).then(function () {
+                req.flash('successUpdate', 'You are successfully updating !', { maxage: 6000 });
+                res.redirect('/admin/jobs-management');
+            });
     });
     //} else {
     //    res.redirect('/login');
